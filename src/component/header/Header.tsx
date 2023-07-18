3e455da1d4eb66fe45c5a0bf508a4b3dc586e622
unclebonn <khoita1008@gmail.com>
2023-07-18 10:06:58 +0700

    update icon for setting

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -4,19 +4,23 @@ import { useSelector, useDispatch } from 'react-redux';
 import "./header.css"
 import { logout, selectInformation, selectRole } from '../../pages/login/loginSlice';
 import { setOpenMenu, setMenuRole } from './headerSlice';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 import NotificationIcon from '../../app/assets/icons/notification.svg';
 import SettingsIcon from '../../app/assets/icons/settings.svg';
-import LogoutIcon from '../../app/assets/icons/logout.svg';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { Divider } from 'antd'
+import { ListItemIcon, ListItemText } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function Header() {
     // Select data from store
     const loginInfo = useSelector(selectInformation);
     const userRole = useSelector(selectRole);
     const dispatch = useDispatch();
+
 
     dispatch(setMenuRole(userRole));
 
@@ -30,6 +34,9 @@ export default function Header() {
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
+    console.log(open);
+    console.log(anchorEl);
+
     const handleClickAccount = (event: React.MouseEvent<HTMLElement>) => {
         event.preventDefault();
         setAnchorEl(event.currentTarget);
@@ -71,7 +78,7 @@ src='https://reqres.in/img/faces/9-image.jpg' />*/}
                     id="account-menu"
                     open={open}
                     onClose={handleClose}
-                    onClick={handleClose}
+                    // onClick={handleClose}
                     PaperProps={{
                         elevation: 0,
                         sx: {
@@ -101,15 +108,23 @@ src='https://reqres.in/img/faces/9-image.jpg' />*/}
                     transformOrigin={{ horizontal: 'right', vertical: 'top' }}
                     anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
                 >
-                    <span style={{ textAlign: "right" }}>
-                        {userRole.normalizedName}</span>
-                    <Divider />
                     <MenuItem onClick={handleLogout}>
-                        <img
-                            src={LogoutIcon}
-                            alt='settings-icon'
-                            className='dashbord-header-icon' />
-                        Logout
+                        <ListItemIcon>
+                            <PersonIcon fontSize='medium'/>
+                        </ListItemIcon>
+                        <ListItemText style={{textAlign:"center"}}>
+                            {userRole.normalizedName}
+                        </ListItemText>
+
+                    </MenuItem>
+                    <Divider style={{margin:"13px 0px"}}/>
+                    <MenuItem onClick={handleLogout}>
+                        <ListItemIcon>
+                            <LogoutIcon fontSize='medium'/>
+                        </ListItemIcon>
+                        <ListItemText style={{textAlign:"center"}}>
+                            Log Out
+                        </ListItemText>
                     </MenuItem>
                 </Menu>
 
